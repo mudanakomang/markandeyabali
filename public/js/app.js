84a4023e938d1cd94860b9995570eca63d8e178f
Mudana <mudana@gruneasia.info>
2021-05-30 23:18:52 +0800

    rebuild

--- public/js/app.js
+++ public/js/app.js
@@ -13062,7 +13062,7 @@ __webpack_require__.r(__webpack_exports__);
 
 vue__WEBPACK_IMPORTED_MODULE_1__.default.use(vue_router__WEBPACK_IMPORTED_MODULE_2__.default);
 var router = new vue_router__WEBPACK_IMPORTED_MODULE_2__.default({
-  mode: 'hash',
+  mode: 'history',
   routes: _routes__WEBPACK_IMPORTED_MODULE_0__.default
 });
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (router);
